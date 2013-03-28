d590d8ce24a225f24bbe98a3b3783ea9730031e5
Adrien Brunet <abrunet@centrale-marseille.fr>
2013-03-28 02:36:02 +0100

    test urls

--- basic/tests.py
+++ basic/tests.py
@@ -1,16 +1,61 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
-
+from django.test.client import Client
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from forum.models import *
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+    def setUp(self):
+        f = Forum.objects.create(title="forum")
+        u = User.objects.create_user("ak", "ak@abc.org", "pwd")
+        Site.objects.create(domain="test.org", name="test.org")
+        t = Thread.objects.create(title="thread", creator=u, forum=f)
+        p = Post.objects.create(title="post", body="body", creator=u, thread=t)
+
+    def test_all_urls(self):
+        self.c = Client()
+        self.c.login(username="ak", password="pwd")
+        resp = self.client.get('/forum/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/admin/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/about/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/forum/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/forum/1/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/thread/1/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/post/new_thread/1/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/post/reply/1/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_all_urls_admin(self):
+        self.c = Client()
+        self.c.login(username="ak", password="pwd")
+        resp = self.client.get('/admin/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/admin/forum/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/admin/forum/post/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/admin/forum/thread/')
+        self.assertEqual(resp.status_code, 200)
+
+
--- forum/tests.py
+++ forum/tests.py
@@ -2,13 +2,9 @@ from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
-
 from forum.models import *
 
-<<<<<<< HEAD
 
-=======
->>>>>>> e0fb891d42881a91bf5afc1e1961cb3a12424c00
 class SimpleTest(TestCase):
     def setUp(self):
         f = Forum.objects.create(title="forum")
@@ -25,7 +21,6 @@ class SimpleTest(TestCase):
             self.assertTrue(v in r.content)
 
     def test(self):
-<<<<<<< HEAD
         ''' Testing creation of forum and thread. '''
         self.c = Client()
         self.c.login(username="ak", password="pwd")
@@ -37,18 +32,3 @@ class SimpleTest(TestCase):
         # r = self.c.post("/post/reply/2/", {})
 #       self.content_test("/thread/2/", ['<tr style="border-collapse : collapse; border-spacing : 2px;" class="odd">','<i style="color:grey;">by ak |','body2'])
         #     ' <td style="padding-left: 20px;">body3</td>'
-=======
-        self.c = Client()
-        self.c.login(username="ak", password="pwd")
-
-        self.content_test("/forum/", ['<a href="/forum/forum/1/">forum</a>'])
-        self.content_test("/forum/forum/1/", ['<a href="/forum/thread/1/">thread</a>', "ak - post"])
-
-        self.content_test("/forum/thread/1/", ['<div class="ttitle">thread</div>',
-               '<span class="title">post</span>', 'body <br />', 'by ak |'])
-
-        r = self.c.post("/forum/new_thread/1/", {"subject": "thread2", "body": "body2"})
-        r = self.c.post("/forum/reply/2/", {"subject": "post2", "body": "body3"})
-        self.content_test("/forum/thread/2/", ['<div class="ttitle">thread2</div>',
-               '<span class="title">post2</span>', 'body2 <br />', 'body3 <br />'])
->>>>>>> e0fb891d42881a91bf5afc1e1961cb3a12424c00
